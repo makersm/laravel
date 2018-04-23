7ae9ee04d7ecd3940fdc2c8e34e371c3d368064b
makersm <sayyo1120@gmail.com>
2018-04-23 22:37:39 +0900

    temporaray commit - make user db seeder - make WelcomeController and router

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,68 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [
+    'as' => 'root',
+    'uses' => 'WelcomeController@index'
+]);
+
+Route::get('home', [
+    'as' => 'home',
+    'uses' => 'WelcomeController@home'
+]);
+
+/* User Registration */
+Route::group(['prefix' => 'auth', 'as' => 'user.'], function() {
+    Route::get('register', [
+        'as' => 'create',
+        'uses' => 'Auth\LoginController@getRegister'
+    ]);
+
+    Route::get('register', [
+        'as' => 'store',
+        'uses' => 'Auth\LoginController@postRegister'
+    ]);
+});
+
+Route::group(['prefix' => 'auth', 'as' => 'session.'], function() {
+    Route::get('login', [
+        'as' => 'create',
+        'uses' => 'Auth\LoginController@getLogin'
+    ]);
+
+    Route::post('login', [
+        'as' => 'store',
+        'uses' => 'Auth\LoginController@postLogin'
+    ]);
+
+    Route::get('logout', [
+        'as' => 'destroy',
+        'uses' => 'Auth\LoginController@getLogout'
+    ]);
+});
+
+/* Password Reminder */
+Route::group(['prefix' => 'password'], function () {
+    Route::get('remind', [
+        'as'   => 'reminder.create',
+        'uses' => 'Auth\ResetPasswordController@getEmail'
+    ]);
+    Route::post('remind', [
+        'as'   => 'reminder.store',
+        'uses' => 'Auth\ResetPasswordController@postEmail'
+    ]);
+    Route::get('reset/{token}', [
+        'as'   => 'reset.create',
+        'uses' => 'Auth\ResetPasswordController@getReset'
+    ]);
+    Route::post('reset', [
+        'as'   => 'reset.store',
+        'uses' => 'Auth\ResetPasswordController@postReset'
+    ]);
 });
 
 Route::get('posts', [
